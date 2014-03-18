c230e94e2c17d5e52f1072c4ec661b4fbe7a6bba
Dean Hillan <dean.hillan@gmail.com>
2014-03-18 20:56:10 +1100

    Working

--- main.jl
+++ main.jl
@@ -8,12 +8,12 @@ function is_prime(n::Int64)
     if n % 2 == 0
         return false
     end
-    for (i in 3:2:floor(sqrt(n))
-        #if n % i == 0
-        #    return false
-        #end
+    for (i in 3:2:floor(sqrt(n)))
+        if n % i == 0
+            return false
+        end
     end
     return true
 end
 
-println(is_prime(7))
+println(is_prime(9))
